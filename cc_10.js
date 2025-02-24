0cf6f1289105f3418c851b1de051f2fb5efc4681
Easton <eastonmullins@usf.edu>
2025-02-23 23:00:04 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -46,3 +46,25 @@ console.log(order1.getOrderDetails());
 
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
+
+
+// Task 3 - Created Inventory Class
+
+class Inventory{
+    constructor(){
+        this.products = [];
+    }
+    addProduct(product){
+        this.products.push(product);
+    }
+    listProducts(){
+        this.products.forEach(product => {
+            console.log(product.getDetails());
+        });
+    }
+}
+const inventory = new Inventory();
+inventory.addProduct(prod1);
+inventory.listProducts();
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+
